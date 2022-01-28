ce06f21d12e17a4ab06f93da5016b492c6c8e237
anil0775 <anilbng75@gmail.com>
2022-01-28 13:17:02 +0530

    First Commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "This is my first Jenkins Project!";
 
     public App() {}
 
